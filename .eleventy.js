cac98befe2daeab8f06b6f277af0e0645d48a747
Santiago De Bernardez <sdebernardez@cerrosoft.com>
2021-10-21 20:35:16 -0300

    Add CSS minify filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
+const CleanCSS = require("clean-css");
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/assets");
